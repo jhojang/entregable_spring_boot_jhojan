3711dc70b6d7d5f1fe7707a83dedea315baae9e3
Jhojan Gomez <jhojan.gomez4@gmailcom>
2020-08-19 03:50:46 -0500

    Nuevo proyecto API-backend con Spring Boot

---
+++ login-spring-boot-backend/src/main/java/com/login/spring/boot/backend/LoginSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.login.spring.boot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringBootBackendApplication.class, args);
+	}
+
+}
---
+++ login-spring-boot-backend/src/test/java/com/login/spring/boot/backend/LoginSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.spring.boot.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
