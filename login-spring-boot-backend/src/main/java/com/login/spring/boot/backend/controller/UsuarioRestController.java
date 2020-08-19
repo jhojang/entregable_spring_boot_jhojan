10c82c0f471c03c59efce9804a0bdaf32f321590
Jhojan Gomez <jhojan.gomez4@gmailcom>
2020-08-19 10:53:45 -0500

    Clase controlador con los métodos mapeados http implementado

---
+++ login-spring-boot-backend/src/main/java/com/login/spring/boot/backend/controller/UsuarioRestController.java
@@ -0,0 +1,63 @@
+package com.login.spring.boot.backend.controller;
+
+import java.util.List;
+
+import com.login.spring.boot.backend.entity.Usuario;
+import com.login.spring.boot.backend.services.IUsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = {""})
+@RestController
+@RequestMapping("/api")
+public class UsuarioRestController {
+    
+    @Autowired
+    IUsuarioService usuarioService;
+
+    @GetMapping("/usuarios")
+    public List<Usuario> listar() {
+        return usuarioService.findAll();
+    }
+
+    @PostMapping("/usuarios/{id}")
+    public Usuario ShowUser(@PathVariable Integer id) {
+        return usuarioService.findById(id);
+    }
+
+    @PostMapping("/usuarios")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario create(@RequestBody Usuario usuario) {
+        return usuarioService.save(usuario);
+    }
+
+    @DeleteMapping("/usuarios/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        usuarioService.delete(id);
+    }
+
+    @PutMapping("/usuarios/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario update(@PathVariable Integer id, @RequestBody Usuario usuario) {
+        Usuario usuarioDevuelto = usuarioService.findById(id);
+        usuarioDevuelto.setUserName(usuario.getUserName());
+        usuarioDevuelto.setName(usuario.getName());
+        usuarioDevuelto.setLastName(usuario.getLastName());
+        usuarioDevuelto.setEmail(usuario.getEmail());
+        usuarioDevuelto.setPassword(usuario.getPassword());
+        return usuarioService.save(usuarioDevuelto);
+    }
+
+}
--- login-spring-boot-backend/src/main/java/com/login/spring/boot/backend/services/UsuarioServiceImp.java
+++ login-spring-boot-backend/src/main/java/com/login/spring/boot/backend/services/UsuarioServiceImp.java
@@ -6,28 +6,35 @@ import com.login.spring.boot.backend.entity.Usuario;
 import com.login.spring.boot.backend.models.dao.IUsuarioDao;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class UsuarioServiceImp implements IUsuarioService {
 
     @Autowired
     IUsuarioDao usuarioDao;
 
     @Override
+    @Transactional(readOnly=true)
     public List<Usuario> findAll() {
         return (List<Usuario>) usuarioDao.findAll();
     }
 
     @Override
+    @Transactional(readOnly=true)
     public Usuario findById(Integer id) {
         return usuarioDao.findById(id).orElse(null);
     }
 
     @Override
+    @Transactional
     public Usuario save(Usuario usuario) {
         return usuarioDao.save(usuario);
     }
 
     @Override
+    @Transactional
     public void delete(Integer id) {
         usuarioDao.deleteById(id);
     }
