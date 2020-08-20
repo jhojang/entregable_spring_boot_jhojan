94d25e085f5a1c11dd9f318f7bf6bd2f807a5d08
Jhojan Gomez <jhojan.gomez4@gmailcom>
2020-08-19 21:42:19 -0500

    Enrutamiento configurado con ReactRouter

---
+++ login-reactjs-frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppRouter } from './components/AppRouter';
+import './App.css';
+
+function App() {
+
+  return (
+    <AppRouter />
+  );
+}
+
+export default App;
---
+++ login-reactjs-frontend/src/components/AppRouter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { MainScreen } from './MainScreen';
+import { LoginScreen } from './LoginScreen';
+import { RegisterScreen } from './RegisterScreen';
+import { HomeScreen } from './HomeScreen';
+
+export const AppRouter = () => {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={MainScreen} />
+            <Route exact path="/login" component={LoginScreen} />
+            <Route exact path="/register" component={RegisterScreen} />
+            <Route exact path="/home" component={HomeScreen} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+    )
+}
