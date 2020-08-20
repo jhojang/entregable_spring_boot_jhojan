ba0f73a68b40188793f8fe935ba26272a5173d75
Jhojan Gomez <jhojan.gomez4@gmailcom>
2020-08-19 21:53:25 -0500

    Cambio de nombre directorio

--- login-reactjs-frontend/src/components/HomeScreen.js
+++ login-reactjs-frontend/src/components/HomeScreen.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export const HomeScreen = () => {
-    return (
-        <div>
-            <h1>Home Screen</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Home Screen</h1>
+    </div>
+  )
 }
