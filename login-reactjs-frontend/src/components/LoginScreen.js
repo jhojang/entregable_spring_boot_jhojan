565a003070677f30cf9453334d12dc2eeaaa7ccd
Jhojan Gomez <jhojan.gomez4@gmailcom>
2020-08-20 01:50:04 -0500

    Diseño de registro terminado

--- login-reactjs-frontend/src/components/LoginScreen.js
+++ login-reactjs-frontend/src/components/LoginScreen.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { RegisterScreen } from './RegisterScreen';
 import loginIcon from './login-icon.png';
 
 export const LoginScreen = () => {
-    return (
-        <div className="global-container form-container">
-            <form>
-                <div className="form-header">
-                <div className="blue-header">
-                    <img src={loginIcon} className="img-header"/>
-                </div>
-                </div>
-                <div className="input-list form-section">
-                    <h1>Iniciar sesión</h1>
-                    <input type="text" name="userName" placeholder="Nombre" />
-                    <input type="password" name="password" placeholder="Contraseña" />
-                </div>
-                <div className="input-list form-footer">
-                    <input type="submit" value="Ingresar" />
-                    <p>¿No tienes cuenta?</p>
-                    <Link to="/registro">Registrate</Link>
-                </div>
-            </form>
+  return (
+    <div className="global-container form-container">
+      <form>
+        <div className="form-header">
+          <div className="blue-header">
+            <img src={loginIcon} className="img-header"/>
+          </div>
         </div>
-    )
+        <div className="input-list form-section">
+          <h1>Iniciar sesión</h1>
+            <input type="text" name="userName" placeholder="Nombre" />
+            <input type="password" name="password" placeholder="Contraseña" />
+        </div>
+        <div className="input-list form-footer">
+          <input type="submit" value="Ingresar" />
+          <p>¿No tienes cuenta?</p>
+          <Link to="/register">Registrate</Link>
+        </div>
+      </form>
+    </div>
+  )
 }
--- login-reactjs-frontend/src/components/RegisterScreen.js
+++ login-reactjs-frontend/src/components/RegisterScreen.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import loginIcon from './login-icon.png';
 
 export const RegisterScreen = () => {
-    return (
-        <div>
-            <h1>Register Screen</h1>
+  return (
+    <div className="global-container form-container">
+      <form>
+        <div className="form-header">
+          <div className="blue-header">
+            <img src={loginIcon} className="img-header"/>
+          </div>
         </div>
-    )
+        <div className="input-list form-section">
+          <h1>Registrarse</h1>
+            <input type="text" name="name" placeholder="Nombre" />
+            <input type="text" name="lastName" placeholder="Apellido" />
+            <input type="text" name="userName" placeholder="Nombre de usuario" />
+            <input type="text" name="email" placeholder="correo electrónico" />
+            <input type="password" name="password" placeholder="Contraseña" />
+        </div>
+        <div className="input-list form-footer">
+          <input type="submit" value="Enviar" />
+          <p>¿No tienes cuenta?</p>
+          <Link to="/login">Inicia sesión</Link>
+        </div>
+      </form>
+    </div>
+  )
 }
